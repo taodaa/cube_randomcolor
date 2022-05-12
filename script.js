a77760e9a5a83cc714f7bdaca93493b64e7ee1f8
taodaa <philipeidolon@vk.com>
2022-05-12 17:34:54 +0500

    v3

--- script.js
+++ script.js
@@ -2,27 +2,44 @@
 // Math.ceil(Math.random()*100) Math.floor- округление вниз. Math.round возвращает ближайшее целое число. 
 // Math.ceil - округление вверх.
 // box[i].id - таким образом можно вывести id элемента NodeLista из цикла и.т.д
+// setInterval(painter, 1000) вызывает функцию с интерваллом.
+// element.style.height = null можно удалять стиль так
+let boxactive = document.querySelector('.icon');
 let box = document.querySelectorAll('.icon'); 
-// let boxactive = document.querySelector('.icon');
 let masiv = {};
 
 
-let circle = setTimeout(function painter(){
+function painter(){
+    onclick.no
     for(i=0; i < box.length; i++){
         let math =  Math.floor(Math.random()*7);
-   if(math === i){
-    masiv = (box[i]);
-    // console.log(masiv);
+        let math2 =  Math.floor(Math.random()*7);
+   if(math === math2){
+    masiv = box[i];
+    // console.log(box[i].id);
     masiv.style.background = "red";
     break;
     }
-    else{
-
-    }
 }
-},5000);
+if(masiv.style.background = "red"){
+    setTimeout(()=>{
+        masiv.style.background = null;
+    },500);
+}
+}
 
 
+masiv.addEventListener('click', ()=>{
+    var count = 0;
+    if(masiv.style.background = "red"){
+        console.log(count++)
+    }
+    else(
+        console.log(count--)
+    )
+});
+
+setInterval(painter, 1000);
 //TODO:надо правильно запустить функцию и сделать интервал, чтобы не спамило в консоль, также нужно перекрашивать обратно блок.;
 // painter();
 
